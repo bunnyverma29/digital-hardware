76bb0e63bbcdfc6ab45f1093b525e7de31d23d9a
dipesh <73181992+bunnyverma29@users.noreply.github.com>
2021-06-11 01:21:08 +0530

    Update design.v

--- 4 bit RCA/design.v
+++ 4 bit RCA/design.v
@@ -13,14 +13,17 @@ module fa(
 	 or o1(carry,w1,w2,w3);
 endmodule
 
-module fa_4bit;
-wire [3:0]a,b;
+module fa_4bit (a,b,cin,sum,cout);
+input [3:0]a,b;
+input cin;
+output [3:0] sum;
 wire [3:0] c;
-wire [3:0] sum;
+output cout;
 
-fa f0 (a[0],b[0],1'b0,sum[0],c[0]);
+
+fa f0 (a[0],b[0],cin,sum[0],c[0]);
 fa f1 (a[1],b[1],c[0],sum[1],c[1]);
 fa f2 (a[2],b[2],c[1],sum[2],c[2]);
-fa f3 (a[3],b[3],c[2],sum[3],c[3]);
+fa f3 (a[3],b[3],c[2],sum[3],cout);
 
 endmodule
